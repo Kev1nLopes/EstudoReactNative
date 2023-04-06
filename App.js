eeed461704984d126f1a8c23eae04491f94f3e6e
Kevin Lopes <kevin.lopes@avmb.com.br>
2023-04-06 11:51:58 -0300

    Iniciando modal

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
 import { StatusBar, StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
-
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 
 const PlaceholderImage = require('./assets/crash.jpg');
 
 export default function App() {
-  
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+    const pickImageAsync = async () =>{
+      let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        quality: 1
+      });
+
+      if(!result.canceled){
+        setSelectedImage(result.assets[0].uri)
+        setShowAppOptions(true);
+      }else{
+        alert('You dit not select any image')
+      }
+    }
   
 
   return (
     <View style={styles.container}>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
       
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label={'Escolha uma foto'} />
-        <Button label={'Usar a foto atual'} />
-      </View>
       
       <StatusBar backgroundColor='black' barStyle={'light-content'}  />
     </View>
